3eb80b26db93c07122999ee8182b436e58dde750
Rushikesh <rushikhandare2203@gmail.com>
2023-01-16 17:23:52 +0530

    Completed data transformation satge

--- sensor/config.py
+++ sensor/config.py
@@ -11,5 +11,7 @@ class EnvironmentVariable:
     aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
     aws_access_secret_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
 
+
 env_var = EnvironmentVariable()
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+TARGET_COLUMN = "class"
