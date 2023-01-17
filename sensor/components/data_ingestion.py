3c1b1bbf65097f1072db8b46179a760c0b11fad8
Rushikesh <rushikhandare2203@gmail.com>
2023-01-17 23:15:25 +0530

    Completed Train Model

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -41,7 +41,8 @@ class DataIngestion:
 
             logging.info("split dataset into train and test set")
             #split dataset into train and test set
-            train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size)#,random_state=42)
+            train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size)
+                                #train_size=self.data_ingestion_config.train_size)#,random_state=42)
             
             logging.info("create dataset directory folder if not available")
             #create dataset directory folder if not available
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -22,13 +22,13 @@ def start_training_pipeline():
 
           # data validation
           data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
-          data_validation = data_ingestion.DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
+          data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
           data_validation_artifact = data_validation.initiate_data_validation()
 
           #data transformation
           data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
           data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
-          data_ingestion_artifact=data_ingestion_artifact)
+                                                    data_ingestion_artifact=data_ingestion_artifact)
           data_transformation_artifact = data_transformation.initiate_data_transformation()
           
           #model trainer
