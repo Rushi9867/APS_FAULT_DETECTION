b1ad80728d656ff9a8ee6c6219bfb25a582f5fbd
Rushikesh <rushikhandare2203@gmail.com>
2023-01-18 14:06:34 +0530

    Project Deployement Configured

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 
 class DataIngestion:
     
-    def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig ):
+    def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig):
         try:
             logging.info(f"{'>>'*20} Data Ingestion {'<<'*20}")
             self.data_ingestion_config = data_ingestion_config
@@ -41,8 +41,7 @@ class DataIngestion:
 
             logging.info("split dataset into train and test set")
             #split dataset into train and test set
-            train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size)
-                                #train_size=self.data_ingestion_config.train_size)#,random_state=42)
+            train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size,random_state=42)
             
             logging.info("create dataset directory folder if not available")
             #create dataset directory folder if not available
