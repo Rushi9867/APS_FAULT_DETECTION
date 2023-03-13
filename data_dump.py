29b72e5da8f41efd08b6beeb0ccecfdc05749b29
Rushikesh Khandare <rushikhandare2203@gmail.com>
2023-03-13 17:46:26 +0530

    Update main.yml

--- data_dump.py
+++ data_dump.py
@@ -2,8 +2,8 @@ import pymongo
 import pandas as pd
 import json
 # Provide the mongodb localhost url to connect python to mongodb.
-#client = pymongo.MongoClient("mongodb://localhost:27017")
-client = pymongo.MongoClient("mongodb+srv://Rushikesh:Rushi9867@cluster0.f3rukrp.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb://localhost:27017")
+#client = pymongo.MongoClient("mongodb+srv://Rushikesh:Rushi9867@cluster0.f3rukrp.mongodb.net/?retryWrites=true&w=majority")
 
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
